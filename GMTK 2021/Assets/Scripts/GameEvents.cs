f7bd33544606054b79352f1abf05b79ff21a2411
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2021-06-03 09:59:20 +0100

    Event Manager

---
+++ GMTK 2021/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current; // Current Event Manager
+    void Awake()
+    {
+        current = this; // Assign Event Manager
+    }
+
+    public event Action onSceneTransition; // New Event
+    public void BeginSceneTransition()
+    {
+        if (onSceneTransition != null) // Check Event Isn't Null
+        {
+            onSceneTransition(); // Invoke Event
+        }
+    }
+}
