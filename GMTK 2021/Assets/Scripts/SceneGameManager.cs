68263cdc874dca29d3ff653ff9daf468ef4cbe8d
SamDoesStuff1 <58668956+SamDoesStuff1@users.noreply.github.com>
2021-06-04 09:01:44 +0100

    2D conversion

---
+++ GMTK 2021/Assets/Scripts/SceneGameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneGameManager : MonoBehaviour
+{
+    public void changeScenes(string name)
+    {
+        SceneManager.LoadScene(name);//load the scene
+    }
+}
