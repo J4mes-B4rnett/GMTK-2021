23c00749aa04cf2940a69c7911096b3817f750b3
SamDoesStuff1 <58668956+SamDoesStuff1@users.noreply.github.com>
2021-06-04 09:36:58 +0100

    Scene and transition manager

--- GMTK 2021/Assets/Scripts/SceneGameManager.cs
+++ GMTK 2021/Assets/Scripts/SceneGameManager.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class SceneGameManager : MonoBehaviour
 {
-    public void changeScenes(string name)
+    public GameObject transition;
+    private Animator transitionAnim;
+
+    private void Start()
     {
-        SceneManager.LoadScene(name);//load the scene
+        //get access to the transition gameObject's animator component
+        transition = GameObject.Find("Transition");
+        transitionAnim = transition.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //This section is just a way to show the transitions - it doesn't do much
+        if (Input.anyKeyDown)
+        {
+            if(SceneManager.GetActiveScene().name == "TempScene1")
+            {
+                StartCoroutine(changeScenes("TempScene2"));
+            }
+            else
+            {
+                StartCoroutine(changeScenes("TempScene1"));
+            }
+        }
+    }
+
+    public IEnumerator changeScenes(string name)
+    {
+        transitionAnim.SetTrigger("EndScene");//start the end of scene animation
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(name);//end the scene
     }
 }
