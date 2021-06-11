112d70cecf6b656537a289f5928c9a0634cd436c
konkey dong <cannkadioglu@gmail.com>
2021-06-11 23:18:47 +0300

    Jump And Movement

---
+++ GMTK 2021/Assets/Scripts/scr_player_jump.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class scr_player_jump : MonoBehaviour {
+
+    //Jump
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float gravity;
+    [SerializeField] private Vector2 groundCheckArea;
+    public bool grounded { get; private set; }
+
+    //Physics Stuff
+    private new Rigidbody2D rigidbody;
+    private new Collider2D collider;
+    private LayerMask staticObjectLayer;
+
+    private bool GroundCheck() {
+        Vector2 bottomOrigin = collider.bounds.center - new Vector3(0, collider.bounds.extents.y);
+        Collider2D hitObject = Physics2D.OverlapArea(bottomOrigin + groundCheckArea / 2, bottomOrigin - groundCheckArea / 2, staticObjectLayer);
+        return hitObject != null;
+    }
+
+    private void Start() {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        collider = gameObject.GetComponent<Collider2D>();
+        staticObjectLayer = LayerMask.GetMask("Static Object");
+    }
+
+    private void FixedUpdate() {
+        grounded = GroundCheck();
+        if (!grounded) rigidbody.velocity += Vector2.down * gravity;
+    }
+
+    private void Update() {
+        if (grounded && Input.GetKeyDown(KeyCode.Z)) {
+            rigidbody.velocity += Vector2.up * jumpSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.Z) && rigidbody.velocity.y > 0) rigidbody.velocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.y / 2);
+    }
+
+    private void OnDrawGizmosSelected() {
+        Collider2D collider = gameObject.GetComponent<Collider2D>();
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(collider.bounds.center - new Vector3(0, collider.bounds.extents.y), groundCheckArea);
+    }
+}
---
+++ GMTK 2021/Assets/Scripts/scr_player_movement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class scr_player_movement : MonoBehaviour {
+
+    //Movement
+    [SerializeField] private float xSpeedLimit;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float deceleration;
+    public int movementDirection { get; private set; }
+
+    //Physics stuff
+    private new Rigidbody2D rigidbody;
+
+    private void Start() {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        movementDirection = Convert.ToInt32(Input.GetKey(KeyCode.RightArrow)) - Convert.ToInt32(Input.GetKey(KeyCode.LeftArrow));
+    }
+
+    private void FixedUpdate() {
+        float xSpeed = rigidbody.velocity.x;
+        if (Mathf.Abs(xSpeed) < xSpeedLimit || movementDirection != Math.Sign(xSpeed)) {
+            rigidbody.velocity += Vector2.right * acceleration * movementDirection; //acceleration
+        }
+
+        if (movementDirection == 0) {
+            if (Mathf.Abs(xSpeed) > 0) rigidbody.velocity -= Vector2.right * deceleration * Math.Sign(xSpeed); //deceleration
+            if (Mathf.Abs(xSpeed) < deceleration) rigidbody.velocity = new Vector2(0, rigidbody.velocity.y); //rounding
+        }
+    }
+}
