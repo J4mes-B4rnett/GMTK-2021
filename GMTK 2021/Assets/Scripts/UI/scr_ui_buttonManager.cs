47b7264b0c0bc0688018ecbd453f81bbd1127aa4
konkey dong <cannkadioglu@gmail.com>
2021-06-11 23:12:02 +0300

    Added Main menu

---
+++ GMTK 2021/Assets/Scripts/UI/scr_ui_buttonManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_ui_button_manager : MonoBehaviour {
+    private void StarGame(string firstLevel) {
+        print("There are no levels yet, so this is just a placeholder");
+    }
+    private void EndGame(string firstLevel) {
+        Application.Quit(0);
+    }
+}
---
+++ GMTK 2021/Assets/Scripts/UI/scr_ui_button_start.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_ui_buttonManager : MonoBehaviour {
+    public void StartGame(string firstLevel) {
+        print("There are no levels yet, so this is just a placeholder");
+    }
+    public void EndGame() {
+        Application.Quit(0);
+    }
+}
