e3ec39cecdc04dda28fb3b1063d2b1c2bfcface6
M1KUS3Q <mikus100626@gmail.com>
2021-06-12 20:04:52 +0200

    Point system added

---
+++ GMTK 2021/Assets/Scripts/PointSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour
+{
+    public int startValue;
+    public int currentValue { get; private set; }
+
+    void Start()
+    {
+        currentValue = startValue;
+    }
+
+
+    void check()
+    {
+        if(currentValue <= 0)
+        {
+            //Die
+        }
+    }
+    
+    public void ChangeValue(int delta)
+    {
+        currentValue += delta;
+        check();
+    }
+}
