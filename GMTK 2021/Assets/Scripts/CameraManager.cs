38070f46f625574e99ece1fa85ec8a6fcd3e1866
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2021-06-12 17:32:07 +0100

    Reworked Camera and prefabbed things

--- GMTK 2021/Assets/Scripts/CameraManager.cs
+++ GMTK 2021/Assets/Scripts/CameraManager.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Transform> targets;
+    public Vector3 offset;
+
+    private Vector3 velocity;
+    public float smoothTime = 0.5f;
+
+    private void LateUpdate()
     {
-        
+        if (targets.Count == 0)
+            return;
+
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3 GetCenterPoint()
     {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
         
+        return bounds.center;
     }
 }
