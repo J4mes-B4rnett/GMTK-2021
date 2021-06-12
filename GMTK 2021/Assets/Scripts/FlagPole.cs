4263ebe8d3916c15e11980d7040ccbc094fd3cea
M1KUS3Q <mikus100626@gmail.com>
2021-06-12 13:30:52 +0200

    Basic level template added

---
+++ GMTK 2021/Assets/Scripts/FlagPole.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class FlagPole : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Vector3Int flagCellID;
+    public List<TileBase> animationFrames;
+    public float timeBetweenframes;
+    void Start()
+    {
+        StartCoroutine(AnimateFlag(timeBetweenframes));
+    }
+    IEnumerator AnimateFlag(float t)
+    {
+        tilemap.SetTile(flagCellID, animationFrames[0]);
+        yield return new WaitForSeconds(t);
+        tilemap.SetTile(flagCellID, animationFrames[1]);
+        yield return new WaitForSeconds(t);
+        tilemap.SetTile(flagCellID, animationFrames[2]);
+        yield return new WaitForSeconds(t);
+        tilemap.SetTile(flagCellID, animationFrames[3]);
+        yield return new WaitForSeconds(t);
+        StartCoroutine(AnimateFlag(t));
+
+    }
+}
