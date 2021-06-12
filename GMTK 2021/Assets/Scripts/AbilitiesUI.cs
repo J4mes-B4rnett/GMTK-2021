795cf722c6c38e89a67afbbc0e64fec9653bcf09
M1KUS3Q <mikus100626@gmail.com>
2021-06-12 14:19:35 +0200

    Started working on dragndrop abilities

---
+++ GMTK 2021/Assets/Scripts/AbilitiesUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class AbilitiesUI : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    [SerializeField] Canvas canvas;
+    CanvasGroup canvasGroup;
+    RectTransform rectTransform;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ GMTK 2021/Assets/Scripts/AbilityHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class AbilityHolder : MonoBehaviour, IDropHandler
+{
+    public List<GameObject> abilities;
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag != null)
+        {
+            abilities.Add(eventData.pointerDrag);
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
