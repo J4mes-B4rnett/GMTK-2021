7a8c47de1023e40d95fce49abd562df71acbaa22
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2021-06-12 17:53:46 +0100

    Pushes Prefab Edits

--- GMTK 2021/Assets/Scripts/ExitTheLevel.cs
+++ GMTK 2021/Assets/Scripts/ExitTheLevel.cs
@@ -18,7 +18,7 @@ public class ExitTheLevel : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if(collision.gameObject.name == "Rabbit" && collision.gameObject.name == "Turtle")
         {
             Debug.Log("Exiting...");
         }
