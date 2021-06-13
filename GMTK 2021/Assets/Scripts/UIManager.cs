259bad0b05eb67fa6e78db640aab256195f65a6e
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2021-06-13 11:47:30 +0100

    Update UIManager

--- GMTK 2021/Assets/Scripts/UIManager.cs
+++ GMTK 2021/Assets/Scripts/UIManager.cs
@@ -18,10 +18,6 @@ public class UIManager : MonoBehaviour
 
     public void LoadLevelMenu()
     {
-        //debug
-        sTC.StartCoroutine(sTC.leaveScene("LevelManager"));
-        return;
-        //end debug
         if (PlayerPrefs.GetInt("Level 1") == 1)
         {
             sTC.StartCoroutine(sTC.leaveScene("LevelManager"));
