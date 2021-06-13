9ce4b21145f63fff494788e1fce48c599f48d3ea
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2021-06-13 08:50:35 +0100

    Pushed Animator

---
+++ GMTK 2021/Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI playButton;
+    public string text1 = "Play";
+    public string text2 = "Play Tutorial";
+
+    public SceneTransitionController sTC;
+
+    void Awake()
+    {
+        playButton.text = PlayerPrefs.GetInt("Tutorial") == 1 ? text1 : text2;
+    }
+
+    public void LoadLevelMenu()
+    {
+        if (PlayerPrefs.GetInt("Tutorial") == 1)
+        {
+            sTC.StartCoroutine(sTC.leaveScene("LevelManager"));
+        }
+        else
+        {
+            sTC.StartCoroutine(sTC.leaveScene("Tutorial"));
+        }
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
